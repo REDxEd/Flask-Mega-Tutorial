caee1a6d27f0ea26cb0f8745549dc5745e9e2449
Edson G. Machado <edsong355@gmail.com>
2022-08-26 00:19:48 -0300

    Templates

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, Word!!!"
+    user={'username':'Edson'} #mock user
+
+    posts=[ #mock posts
+        {
+            'author':{'username':'João'},
+            'body':'belo dia!'
+        },
+        {
+            'author':{'username':'Susan'},
+            'body':'O filme dos vingadores foi incrivel!!'
+        }
+    ]
+
+    return render_template("index.html", title='Home',user=user, posts=posts)
